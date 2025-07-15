ef67e113163b4232618bdedb5261828d55823a99
Christina <christinaraganit@gmail.com>
2025-07-15 00:31:17 -0700

    Update quiz-context.tsx

--- src/app/quiz-context.tsx
+++ src/app/quiz-context.tsx
@@ -1,19 +1,19 @@
 "use client";
-import {createContext, useContext, useState, FC, ReactNode} from 'react';
-import {MBTI} from "@/app/questions";
+import { createContext, useContext, useState, FC, ReactNode } from "react";
+import { MBTI } from "@/app/questions";
 
 type QuizContextType = {
   answers: Map<MBTI, number>;
-  setAnswers: (newAnswers: Map<MBTI, number>) => void;
-}
+  setAnswers: React.Dispatch<React.SetStateAction<Map<MBTI, number>>>;
+};
 
-const QuizContext = createContext<QuizContextType>(null);
+const QuizContext = createContext<QuizContextType | null>(null);
 
-export const QuizProvider: FC<{children: ReactNode}> = ({children}) => {
+export const QuizProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const [answers, setAnswers] = useState<Map<MBTI, number>>(new Map());
 
   return (
-    <QuizContext.Provider value={{answers, setAnswers}}>
+    <QuizContext.Provider value={{ answers, setAnswers }}>
       {children}
     </QuizContext.Provider>
   );
@@ -22,9 +22,7 @@ export const QuizProvider: FC<{children: ReactNode}> = ({children}) => {
 export const useQuizContext = () => {
   const context = useContext(QuizContext);
   if (!context) {
-    throw new Error('useQuizContext must be used within a QuizProvider');
+    throw new Error("useQuizContext must be used within a QuizProvider");
   }
   return context;
-}
-
-
+};
