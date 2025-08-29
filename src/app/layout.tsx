4f075baf15d55d6bac8bd3e6723629567a5a0ab5
Christina <christinaraganit@gmail.com>
2025-08-28 21:19:32 -0700

    fix metadata hopefully

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,23 @@ import { QuizProvider } from "@/app/quiz-context";
 import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://big-way-mbti.pages.dev/"),
   title: "Big Way Hot Pot MBTI Quiz",
-  description: "Discover which of Big Way's delicious soup bases fits your personality and MBTI the best!",
+  description:
+    "Discover which of Big Way's delicious soup bases fits your personality and MBTI the best!",
+  openGraph: {
+    title: "Big Way Hot Pot MBTI Quiz",
+    description:
+      "Discover which of Big Way's delicious soup bases fits your personality and MBTI the best!",
+    images: [
+      {
+        url: "/opengraph-image.png", // hosted in /public
+        width: 1200,
+        height: 600,
+        alt: "Big Way Hot Pot MBTI Quiz",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
